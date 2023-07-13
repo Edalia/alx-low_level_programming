cea98aebe0b675a08dae517305f1af91f9cb8d95
Edalia <edaliamk@gmail.com>
2023-07-13 23:50:35 +0300

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /*
- *main : Entry point or program
+ *main - Entry point
+ *
+ *Description : prints out the string "Programming is like building a multilingual puzzle"
+ *@parameter : void
  *
  *Return: Always 0 (success)
  */
 
 int main(void)
 {
-	/**
-	 * prints out string
-	 */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
