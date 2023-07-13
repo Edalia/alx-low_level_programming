e61a16ca3ab101503f493292610ffb8e277b397b
Edalia <edaliamk@gmail.com>
2023-07-13 21:35:01 +0300

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
-#include <studio.h>
+#include <stdio.h>
 
 int main(){
-	puts="Programming is like building a multilingual puzzle";
+	puts("Programming is like building a multilingual puzzle");
 	return 0;
 }
