3d3ce8ce77e68ce929686401d58476341c81e50e
Edalia <edaliamk@gmail.com>
2023-07-19 00:12:37 +0300

    commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,8 +15,8 @@ void print_to_98(int n)
 		while (n <= 98)
 		{
 			printf("%d", n);
-			
-			if ( n < 98)
+
+			if (n < 98)
 			{
 				printf(", ");
 			}
@@ -31,7 +31,8 @@ void print_to_98(int n)
 		{
 			printf("%d", n);
 
-			if (n > 98){
+			if (n > 98)
+			{
 				printf(", ");
 			}
 
