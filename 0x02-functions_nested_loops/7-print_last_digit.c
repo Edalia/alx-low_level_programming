7fb3d361dd2bacb5dd3a78d9c4b0b08ef5eeb65f
Edalia <edaliamk@gmail.com>
2023-07-19 16:50:02 +0300

    commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *print_last_digit - prints last integer
+ *@i: params to print last int
+ *Return: (int) last integer
+ */
+
+int print_last_digit(int i)
+{
+	return (i % 10);
+}
