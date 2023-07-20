f3a103020dd4df26e4477d8ad4fb9004f6cc5d6d
Edalia <edaliamk@gmail.com>
2023-07-21 01:46:54 +0300

    commit

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -8,13 +8,19 @@
  *
  *Return: 0 (success)
  */
-/*int main(void)*/
-void positive_or_negative(int i)
+int main(void)
 {
-	if (i < 0)
-		printf("%d is negative\n", i);
-	else if (i == 0)
-		printf("%d is zero\n", i);
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n < 0)
+		printf("%d is negative\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
 	else
-		printf("%d is positive\n", i);
+		printf("%d is positive\n", n);
+
+	return (0);
 }
