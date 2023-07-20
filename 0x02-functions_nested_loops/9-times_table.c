82c09df646b502e9c8c5294f899305e8bee90f57
Edalia <edaliamk@gmail.com>
2023-07-20 04:13:29 +0300

    commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,15 +1,21 @@
 #include "main.h"
-
+/**
+ *times_table - print multiplication table 1-9
+ *
+ *Return: void
+ */
 void times_table(void)
-{	
+{
 	int r = 0;
+
 	while (r < 10)
 	{
 		int c = 0;
-		while (c < 10){
-			int s = r*c;
-				if (s > 9)
-					_putchar('0' + (s / 10));
+
+		while (c < 10)
+		{
+			int s = r * c;
+			_putchar('0' + (s / 10));
 			_putchar('0' + (s % 10));
 			_putchar(',');
 
