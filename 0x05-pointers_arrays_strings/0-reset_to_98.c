5ceea547a50812710e29ac072f733cc4ed00bd92
Edalia <edaliamk@gmail.com>
2023-07-24 21:42:30 +0300

    commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  *reset_to_98 - reset value of the address pointed
- *@n - pointer of address to change
+ *@n: pointer of address to change
  *Return: void
  */
 void reset_to_98(int *n)
