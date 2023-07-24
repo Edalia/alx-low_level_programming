f2a4084779ea66c2eb30b0a5da655cfd9d80b55c
Edalia <edaliamk@gmail.com>
2023-07-25 01:59:39 +0300

    commit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -3,23 +3,18 @@
 
 void puts_half(char *str)
 {
-	int len = (strlen(str) - 1);
+	int len = strlen(str);
+	int half;
 
 	if (len % 2 == 0)
-	{
-		while ((len / 2) > len)
-		{
-			_putchar(str((len / 2)));
-			(len / 2)++;
-		}
-	}
+		half = (len / 2);
 	else
+		half = (len - 1) / 2;
+
+	while (half < len )
 	{
-		while ( ((len + 1) / 2) > len)
-		{
-			_putchar(str(((len + 1) / 2)));
-			((len + 1) / 2)++;
-		}
+		_putchar(str[half]);
+		half++;
 	}
 
 	_putchar('\n');
