e2f9425b5701d5b58fb27aaf28e9c55466239330
Edalia <edaliamk@gmail.com>
2023-07-25 01:50:00 +0300

    commit

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,12 @@
 #include "main.h"
 #include <string.h>
-
+/**
+ *_strcpy - copy src string to dst string
+ *@dest: string to paste to
+ *@src: string to copy
+ *Return: string
+ */
 char *_strcpy(char *dest, char *src)
 {
-	return (strcpy (dest, src));
+	return (strcpy(dest, src));
 }
