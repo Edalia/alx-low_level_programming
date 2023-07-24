7a0ab539da811cc383f3c3959d357aeadc91e13b
Edalia <edaliamk@gmail.com>
2023-07-24 21:40:00 +0300

    commit

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,11 @@
 #include "main.h"
 #include <string.h>
-
+/**
+ *_strlen - finds length of string
+ *@s: pointer of type char
+ *Return: int (length)
+ */
 int _strlen(char *s)
 {
-	return strlen(s);	
+	return (strlen(s));
 }
