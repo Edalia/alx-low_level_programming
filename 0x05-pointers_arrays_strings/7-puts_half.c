0836c14be863435106fb6d4a5b199ea4f8cb7663
Edalia <edaliamk@gmail.com>
2023-07-25 02:10:12 +0300

    commit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,6 +1,10 @@
 #include "main.h"
 #include <string.h>
-
+/**
+ *puts_half - prints half of the string
+ *@str: string to print half
+ *Return: void
+ */
 void puts_half(char *str)
 {
 	int len = strlen(str);
@@ -11,7 +15,7 @@ void puts_half(char *str)
 	else
 		half = (len - 1) / 2;
 
-	while (half < len )
+	while (half < len)
 	{
 		_putchar(str[half]);
 		half++;
