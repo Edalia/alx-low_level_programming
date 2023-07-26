9fd8127c331a4b7373c4a5e9b19b83e74ff029ed
Edalia <edaliamk@gmail.com>
2023-07-26 21:08:57 +0300

    commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,11 @@
 #include "main.h"
 #include <string.h>
-
+/**
+ *_strcat - concantenate strings
+ *@dest: string 2
+ *@src: string 1
+ *Return: char array
+ */
 char *_strcat(char *dest, char *src)
 {
 	return (strcat(dest, src));
