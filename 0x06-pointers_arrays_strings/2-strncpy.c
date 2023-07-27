bbb71a0b82e38ac41383e8dea8026466b80c2a40
Edalia <edaliamk@gmail.com>
2023-07-27 23:44:38 +0300

    commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,9 +1,15 @@
 #include "main.h"
 #include <string.h>
-
+/*
+ *_strncpy - copies n bytes of string from src to dest 
+ *@dest: string to copy to
+ *@src: string to copy from
+ *@n: number of bytes
+ *Return: char (dest)
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
 	strncpy(dest, src, n);
 
-	return dest;
+	return (dest);
 }
