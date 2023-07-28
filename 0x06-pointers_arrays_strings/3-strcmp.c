227f6bc438df3e680c7fb8187acec7b5e66f441d
Edalia <edaliamk@gmail.com>
2023-07-28 19:20:11 +0300

    commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,15 +9,5 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	if (s1 > s2)
-		return (strcmp(s2, s1));
-
-	else if (s1 == s2)
-		return (strcmp(s2, s1));
-
-	else if (s1 < s2)
-		return (strcmp(s2, s1));
-
-	else
-		return (0);
+	return (strcmp(s1, s2));
 }
