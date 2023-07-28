bc9d2979ac3eca05a894aa9ed4f3b00a8e3dc6aa
Edalia <edaliamk@gmail.com>
2023-07-28 19:18:00 +0300

    commit

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,8 +1,13 @@
 #include "main.h"
-
+#include <string.h>
+/**
+ *_strncat - concantenate string but use n bytes of s2
+ *@dest: s1 string
+ *@src: s2 string
+ *@n: bytes
+ *Return: char
+ */
 char *_strncat(char *dest, char *src, int n)
-{	
-	char 
-
-	return (strcat(dest, src));
+{
+	return (strncat(dest, src, n));
 }
