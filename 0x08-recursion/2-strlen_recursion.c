0c90cb6246c55f7f93531e4ccf0f13c2c06eecc6
Edalia <edaliamk@gmail.com>
2023-08-02 17:07:04 +0300

    commit

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,7 +1,11 @@
 #include "main.h"
 #include <string.h>
-
+/**
+ *_strlen_recursion - return number of char in string
+ *@s: string
+ *Return: int (number of char)
+ */
 int _strlen_recursion(char *s)
 {
-	return strlen(s);
+	return (strlen(s));
 }
