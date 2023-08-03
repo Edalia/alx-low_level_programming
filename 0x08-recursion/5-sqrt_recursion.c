0c7fbc3eed887ab6ce460d2488ae0d0cb88cc843
Edalia <edaliamk@gmail.com>
2023-08-03 21:58:02 +0300

    commit

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+int _sqrt_recursion(int n)
+{
+	if ( n < 0)
+		return 0;
+	else (n % 2 == 0)
+
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+int is_prime_number(int n)
+{
+	if (n == 1)
+		return (1);
+	else if (n > 1)
+		
+	else
+		return (0);
+}
